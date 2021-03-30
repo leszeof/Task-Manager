301b9125c5a6de8eb206581a9bac1b846223b403
Nikita Volkhonskiy <literature198@gmail.com>
2021-03-30 19:00:48 +0400

    feat: configurate constructor for Task class and set some basic functions of this class

--- scripts/components/Task.js
+++ scripts/components/Task.js
@@ -1,5 +1,18 @@
 export default class Task {
-  constructor() {
-
+  constructor({title, description, year, month, date, time}) {
+    this._title = title;
+    this._description = description;
+    this._year = year;
+    this._month = month;
+    this._date = date;
+    this._time = time;
   }
+
+  getFullTaskData() {}
+
+  _generateTaskDate() {}
+
+  _generateHashCode() {}
+
+  _hashCode(data) {}
 }
