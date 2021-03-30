56d68547baa232b9ae1dede9531cd64168fb5850
Nikita Volkhonskiy <literature198@gmail.com>
2021-03-30 18:19:13 +0400

    feat: add constants for add new task popup

---
+++ scripts/utils/constants.js
@@ -0,0 +1,13 @@
+// Add new task popup
+const newTaskPopup = document.querySelector('.popup_new-task');
+const openNewTaskPopupButton = document.querySelector('.header__add-button');
+const closeNewTaskPopupButton = newTaskPopup.querySelector('.popup__close-button');
+const newTaskFormElement = newTaskPopup.querySelector('.popup__new-task-form');
+const topicInput = newTaskFormElement.querySelector('.popup__topic');
+const descriptionInput = newTaskFormElement.querySelector('.popup__description');
+const dateInput = newTaskFormElement.querySelector('.popup__date');
+const monthInput = newTaskFormElement.querySelector('.popup__month');
+const yearInput = newTaskFormElement.querySelector('.popup__year');
+const timeInput = newTaskFormElement.querySelector('.popup__time');
+const colorInput = newTaskFormElement.querySelector('.popup__color');
+const colorPseudoInput = newTaskFormElement.querySelector('.popup__pseudo-input');
