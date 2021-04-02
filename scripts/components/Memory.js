4d9f3150fdfc6e75658236c0e2e71dcc99adbf37
Nikita Volkhonskiy <literature198@gmail.com>
2021-04-02 09:58:04 +0400

    feat: add and configure init function to Memory class

--- scripts/components/Memory.js
+++ scripts/components/Memory.js
@@ -5,4 +5,14 @@ export default class Memory {
 
     this.init();
   }
+
+  init() {
+    // возьмет из localStorage сохраненный массив тасков по ключу calendarMemory
+    // если его там нет (ключа или значения) - создаем пустой массив
+    const tasksArr = localStorage.getItem('calendarMemory') || '[]';
+
+    // запишет взятый массив в нашу временную память
+    // или поставит пустой массив как стартовый
+    this._dataTasksArray = JSON.parse(tasksArr);
+  }
 }
