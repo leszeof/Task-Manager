717f9b36903bcbac716967b3b81764f602dfb635
Nikita Volkhonskiy <literature198@gmail.com>
2021-04-02 18:50:21 +0400

    refactor: create _popupSettingsBinding for better vizualization of PopupPreview constructor things

--- scripts/components/PopupPreview.js
+++ scripts/components/PopupPreview.js
@@ -3,11 +3,21 @@ export default class PopupPreview extends Popup {
   constructor({popupSelector}) {
     super(popupSelector);
 
+    this._popupSettingsBinding();
+  }
+
+  _popupSettingsBinding() {
+    // popup elems
     this._popupTitle = this._popupElem.querySelector('.popup-event-preview__title');
     this._popupDate = this._popupElem.querySelector('.popup-event-preview__preview-date');
     this._popupTime = this._popupElem.querySelector('.popup-event-preview__preview-time');
     this._popupDescription = this._popupElem.querySelector('.popup-event-preview__event-description');
+    this._completeCheckboxLabel = this._popupElem.querySelector('.popup-event-preview__label');
     this._deleteButton = this._popupElem.querySelector('.popup-event-preview__delete-button');
-    this._completeButton = this._popupElem.querySelector(".task-cards__complete-button");
+    this._completeButton = this._popupElem.querySelector('.task-cards__complete-button');
+
+    // classes
+    this._completeCheckboxActiveClass = 'task-cards__complete-button_active';
+    this._completeLabelActiveClass = 'task-cards__task_complete';
   }
 }
