38e54b7dafaa170c0e27087f14db5cafefc34550
Nikita Volkhonskiy <literature198@gmail.com>
2021-04-02 13:55:59 +0400

    feat: add open and close functions for Popup class

--- scripts/components/Popup.js
+++ scripts/components/Popup.js
@@ -4,4 +4,20 @@ export default class Popup {
     this._closeButtonElem = this._popupElem.querySelector('.popup__close-button');
 
   }
+
+  // open popup
+  open() {
+    this._popupElem.classList.add('popup_opened');
+
+    // close popup on escape listener
+    document.addEventListener('keydown', this._handleEscClose);
+  }
+
+  // close popup
+  close() {
+    this._popupElem.classList.remove('popup_opened');
+
+    // delete close popup on escape listener
+    document.removeEventListener('keydown', this._handleEscClose);
+  }
 }
