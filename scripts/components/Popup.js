58d024ada7f48f1490bcba6b1c3d56cf6acade50
Nikita Volkhonskiy <literature198@gmail.com>
2021-04-02 13:58:23 +0400

    feat: add setEventListeners, _closeOnOverlayClick and _handleEscClose functions to Popup class

--- scripts/components/Popup.js
+++ scripts/components/Popup.js
@@ -20,4 +20,28 @@ export default class Popup {
     // delete close popup on escape listener
     document.removeEventListener('keydown', this._handleEscClose);
   }
+
+  // close popup on 'escape' press
+  _handleEscClose(event) {
+    if (event.key === 'Escape') {
+      const curentOpenedPopup = document.querySelector('.popup_opened');
+      this.close(curentOpenedPopup);
+    }
+  }
+
+  // set eventlisteners on popup
+  setEventListeners() {
+    // close popup on overlay click listener
+    this._popupElem.addEventListener('click', this._closeOnOverlayClick);
+
+    // close popup on close button listener
+    this._closeButtonElem.addEventListener('click', this.close.bind(this));
+  }
+
+  // close popup on overlay click
+  _closeOnOverlayClick(event) {
+    if (event.currentTarget == event.target) {
+      this.close();
+    }
+  }
 }
