a98a92c1fd32453621ee835ac00a2125d56eb2d5
Nikita Volkhonskiy <literature198@gmail.com>
2021-04-02 10:03:32 +0400

    feat: add main memory functions _saveTaskToLocalMemory and _updateLocalStorage to Memory class. Works in pair with saveTask

--- scripts/components/Memory.js
+++ scripts/components/Memory.js
@@ -16,16 +16,26 @@ export default class Memory {
     this._dataTasksArray = JSON.parse(tasksArr);
   }
 
+  // публичный метод будет отдавать функциям рендеринга массив, чтобы они не лазили в него
+  getCurrentTasksArray() {
+    const arrayOfTasks = localStorage.getItem('calendarMemory') || '[]';
+    // console.log(arrayOfTasks);
+    return JSON.parse(arrayOfTasks);
+  }
+
   saveTask(newTask) {
     this._saveTaskToLocalMemory(newTask);
 
     this._updateLocalStorage();
   }
 
-  // публичный метод будет отдавать функциям рендеринга массив, чтобы они не лазили в него
-  getCurrentTasksArray() {
-    const arrayOfTasks = localStorage.getItem('calendarMemory') || '[]';
-    // console.log(arrayOfTasks);
-    return JSON.parse(arrayOfTasks);
+  _saveTaskToLocalMemory(currentTask) {
+    this._dataTasksArray.push(currentTask);
+  }
+
+  _updateLocalStorage() {
+    // присваиваем наш оперативный массив в localStorage для уже созданного ключа
+    // к сожалению придется гонять массив this._dataTasksArray постоянно туда-сюда
+    localStorage.setItem('calendarMemory', JSON.stringify(this._dataTasksArray));
   }
 }
